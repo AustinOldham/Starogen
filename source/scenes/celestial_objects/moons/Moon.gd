388ffc79624a609b592666803b735792c4620cd5
AustinOldham <tech@oldham.cc>
2019-08-11 20:40:30 -0500

    Started working on the moon

--- source/scenes/celestial_objects/moons/Moon.gd
+++ source/scenes/celestial_objects/moons/Moon.gd
@@ -1,12 +1,11 @@
 extends CelestialBody
 
 onready var planet_generator = preload("res://plugins/PlanetGenerator/bin/PlanetGenerator.gdns").new()
-const Moon = preload("res://scenes/celestial_objects/moons/Moon.tscn")
 
 func _ready():
 	$GravitationalField.connect("body_entered", self, "_on_GravitationalField_body_enter")
 	$GravitationalField.connect("body_exited", self, "_on_GravitationalField_body_exit")
-	size = 100
+	size = 101
 	randomize()
 	seed_input = int(rand_range(0, 2147483647))
 	#scale = Vector2(5, 5)
@@ -15,17 +14,9 @@ func _ready():
 	get_node("GravitationalField/CollisionShape2D").shape.radius = size
 	get_node("GravitationalField").scale = scale
 	$Sprite.scale = scale
-	_add_moon()
-	print(get_child_count())
 
-func _add_moon():
-	print("Moon creation")
-	var some_moon = Moon.instance()
-	add_child(some_moon)
-	some_moon.is_working()
-	print("Moon path: ", some_moon.get_path())
-	print("Moon size: ", some_moon.size)
-	#print(some_moon.get_child_count())
+func adjust_scale(new_scale):
+	pass
 
 func _process(delta):
 	rotation_degrees += 2.0 * delta
@@ -37,10 +28,14 @@ func _generate():
 	$Sprite.texture = planet_generator.getPlanet(seed_input, size)	
 	_create_collision_polygon()
 
+
 func _on_GravitationalField_body_enter(body):
 	if (body.has_method("_set_gravity")):
 		body._set_gravity(self)
 
 func _on_GravitationalField_body_exit(body):
 	if (body.has_method("_remove_gravity")):
-		body._remove_gravity(self)	
+		body._remove_gravity(self)
+
+func is_working():
+	print("Moon is working")
